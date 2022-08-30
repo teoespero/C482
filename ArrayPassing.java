b30e0e4ce0576388e3d3c4ddbde32426256be49d
Teo Espero <38012835+teoespero@users.noreply.github.com>
2022-08-30 15:25:50 -0700

    Add files via upload

---
+++ ArrayPassing.java
@@ -0,0 +1,72 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  Teo Espero
+//  BS Cloud and Systems Administration
+//  BS Software Development
+//  Western Governors University
+//  ArrayPassing -  this Java program was created to demonstrate passing arrays to methods
+//  Date written: 08 30 2022
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//  for input functions
+import java.util.Scanner;
+
+//  array function
+import java.util.Arrays;
+
+//  data mismatch
+import java.util.InputMismatchException;
+
+public class ArrayPassing {
+    public static void main(String[] args){
+        int[] licensePlates = new int[10];
+
+        getPlates(licensePlates);
+        System.out.println("Licenses entered (unsorted): ");
+        showPlates(licensePlates);
+        licensePlates = sortTheArrays(licensePlates);
+        System.out.println("Licenses entered (sorted): ");
+        showPlates(licensePlates);
+    }
+
+    //  method for getting license plates
+    public static void getPlates(int[] licensePlateHolder){
+        Scanner userInput = new Scanner(System.in);
+
+        //  greet the user
+        System.out.println("Acme License plates ");
+        System.out.println("----------------------------");
+        System.out.println("Enter the licenses below: ");
+
+        //  loop and ask for license plates
+        for (int ptr = 0; ptr < licensePlateHolder.length; ptr++){
+
+            //  check for bad input data
+            try{
+                System.out.print(ptr + 1 + " -> ");
+                licensePlateHolder[ptr] =  userInput.nextInt();
+            }
+            //  if it fails, throw out an exception error
+            catch (InputMismatchException ex){
+                System.out.println("Invalid license plate!");
+                userInput.next();
+                ptr--;
+            }
+        }
+    }
+
+    //  methods for showing the plates gathered (unsorted)
+    public static void showPlates(int[] licensePlateHolder){
+        for (int thePlates: licensePlateHolder) {
+            System.out.println(thePlates);
+        }
+    }
+
+    //  methods for showing the plates gathered (sorted)
+    public static int[] sortTheArrays(int[] licensePlateHolder){
+        Arrays.sort(licensePlateHolder);
+        return  licensePlateHolder;
+    }
+
+}
+
+
